5f190858834aafcd7543f025d6fb956404059ad2
Sebastian906 <elsebas1912@gmail.com>
2025-08-12 21:14:37 -0500

    hola

--- src/components/pokemons/pokemon-image.tsx
+++ src/components/pokemons/pokemon-image.tsx
@@ -1,21 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 interface Props {
-    id   : number;
+    id: number;
     size?: number;
     backImage: boolean;
 }
 
-export const PokemonImage = component$(( {id, size = 200, backImage = false }: Props ) => {
+export const PokemonImage = component$(({ id, size = 200, backImage = false }: Props) => {
+    
+    const imageLoaded = useSignal(false);
+    
     let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     if (backImage) {
         imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
     }
     return (
-        <img 
-            src={imageUrl} 
-            alt="Imagen de Pokemon" 
-            style={{ width: `${size}` }}
-        />
+        <div class="flex items-center justify-center"
+            style={{ width: `${size}px`, height: `${size}px` }}>
+            <span>Cargando...</span>
+            <img
+                src={imageUrl}
+                alt="Imagen de Pokemon"
+                style={{ width: `${size}` }}
+            />
+        </div>
     )
 })
